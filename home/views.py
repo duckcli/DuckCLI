b364bb7e5d62c647e6a73aadfd803a4bdf137077
Prathap Shetty <shettypratap75@gmail.com>
2022-11-06 22:03:06 +0000

    delete v1

--- home/views.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.contrib.auth.decorators import login_required
-
-from django.shortcuts import render
-
-@login_required
-def home(request):
-    return render(request, 'home.html')
